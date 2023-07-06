d4f356cdf3e1c4101037de1e77793900e39de0a6
SafaraliAbduazimov <02safmath05@gmail.com>
2023-07-06 16:39:13 +0500

    I used  factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Customer;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,15 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-//        Customer::create([
-//            'id'=>1,
-//            'full_name' => 'Абдуазимов Сафарали',
-//            'address' => 'Dushanbe',
-//            'phone' => '+992 93 722 05 02',
-//            'email' => '02safmath05@gmail.com',
-//            'password' => Hash::make('0508'),
-//        ]);
-
+        $this->call([
+           CustomerSeeder::class
+        ]);
     }
 }
