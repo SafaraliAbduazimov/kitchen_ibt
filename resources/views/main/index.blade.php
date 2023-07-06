edf3b715f9e6570126d3aa814f75a5bd992692de
SafaraliAbduazimov <02safmath05@gmail.com>
2023-07-06 11:33:06 +0500

    I added bootstap

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Kitchen of IBT </title>
-</head>
-<body>
-    <h1>Kitchen of IBT</h1>
-</body>
-</html>
+@extends('layout.index')
+
+@section('title','this is Main.index')
+
+@section('content')
+    <div class="container mt-5">
+        <h1 class="alert-danger">Salom ibt</h1>
+    </div>
+@endsection
